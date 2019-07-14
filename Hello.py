64a171c1ad1c9aade39fbb17b7988ac960c99ff4
Lucien Michael <lucien@MAL>
2019-07-14 23:42:58 +0800

    数据结构-列表推导式OK

--- Hello.py
+++ Hello.py
@@ -730,4 +730,72 @@ def outer():
     print(num)
 outer()
 
+
+## Python数据结构
+# 列表list
+'''
+list.append(x) 	把一个元素添加到列表的结尾，相当于 a[len(a):] = [x]。
+list.extend(L) 	通过添加指定列表的所有元素来扩充列表，相当于 a[len(a):] = L。
+list.insert(i, x) 	在指定位置插入一个元素。第一个参数是准备插入到其前面的那个元素的索引，
+例如 a.insert(0, x) 会插入到整个列表之前，而 a.insert(len(a), x) 相当于 a.append(x) 。
+list.remove(x) 	删除列表中值为 x 的第一个元素。如果没有这样的元素，就会返回一个错误。
+list.pop([i]) 	从列表的指定位置移除元素，并将其返回。如果没有指定索引，a.pop()返回最后一个元素。
+元素随即从列表中被移除。（方法中 i 两边的方括号表示这个参数是可选的，而不是要求你输入一对方括号，
+你会经常在 Python 库参考手册中遇到这样的标记。）
+list.clear() 	移除列表中的所有项，等于del a[:]。
+list.index(x) 	返回列表中第一个值为 x 的元素的索引。如果没有匹配的元素就会返回一个错误。
+list.count(x) 	返回 x 在列表中出现的次数。
+list.sort() 	对列表中的元素进行排序。
+list.reverse() 	倒排列表中的元素。
+list.copy() 	返回列表的浅复制，等于a[:]。 
+'''
+
+# 将列表当做堆栈使用
+# 列表方法使得列表可以很方便的作为一个堆栈来使用，堆栈作为特定的数据结构，最先进入的元素最后一个被释放（后进先出）。
+# 用 append() 方法可以把一个元素添加到堆栈顶。用不指定索引的 pop() 方法可以把一个元素从堆栈顶释放出来。
+stack = [1,2,3]
+stack.append(4)
+stack.append(5)
+stack
+stack.pop()
+stack
+stack.pop()
+stack.pop()
+stack
+
+# 将列表当做队列使用
+# 也可以把列表当做队列用，只是在队列里第一加入的元素，第一个取出来；但是拿列表用作这样的目的效率不高。
+# 在列表的最后添加或者弹出元素速度快，然而在列表里插入或者从头部弹出速度却不快（因为所有其他的元素都得一个一个地移动）。 
+from collections import deque
+queue=deque(['Eric', 'John', 'Michael'])
+queue.append('Terry')
+queue.append('Graham')
+queue
+queue.popleft()
+queue.popleft()
+queue
+
+# 列表推导式
+vec = [1,2,3]
+[3*x for x in vec]
+
+[[x,x**2] for x in vec]
+[(x,x**3) for x in vec]
+
+freshfruit = ['  banana', '  loganberry ', 'passion fruit  ']
+[weapon.strip() for weapon in freshfruit]
+
+# 可以用if子句作为过滤器
+[3*x for x in vec if x > 1]
+[3*x for x in vec if x < 2]
+
+# 关于循环和其它技巧
+vec1 = [2, 4, 6]
+vec2 = [4, 3, -9]
+[x*y for x in vec1 for y in vec2] # 先后面的循环在前面的循环
+[x+y for x in vec1 for y in vec2]
+[vec1[i]*vec2[i] for i in range(len(vec1))]
+
+# 列表推导式可以使用复杂表达式或嵌套函数
+[str(round(355/113, i)) for i in range(1, 6)]
 """##全文引号结束
