1a7a001a0af846dae9031f7ca2142d516cc46929
Lucien Michael <lucien@MAL>
2020-02-11 21:52:44 +0800

    PythonIO-pickle

--- Hello.py
+++ Hello.py
@@ -923,7 +923,7 @@ print('命令行参数如下:')
 for i in sys.argv:
    print(i)
  
-print('\n\nPython 路径为：', sys.path, '\n')
+print('\nPython 路径为：', sys.path, '\n')
 
 # 如果你打算经常使用一个函数，你可以把它赋给一个本地的名称
 fib = fibo.fib
@@ -981,7 +981,7 @@ print('Python!')
 ### Python3输入和输出
 ## 输出格式美化
 # str()： 函数返回一个用户易读的表达形式。 
-# epr()： 产生一个解释器易读的表达形式。
+# repr()： 产生一个解释器易读的表达形式。
 s = "Hello, Python"
 str(s)
 repr(s)
@@ -1186,4 +1186,31 @@ with open("foo.txt",'r') as f:
 f.closed
 # 文件对象还有其他方法, 如 isatty() 和 trucate(), 但这些通常比较少用。
 
+
+## pickle模块
+# python的pickle模块实现了基本的数据序列和反序列化。
+# 通过pickle模块的序列化操作我们能够将程序中运行的对象信息保存到文件中去，永久存储。
+# 通过pickle模块的反序列化操作，我们能够从文件中创建上一次程序保存的对象。 
+# 基本接口： pickle.dump(obj, file, [,protocol])
+# 有了 pickle 这个对象, 就能对 file 以读取的形式打开: x = pickle.load(file)
+
+import pickle
+
+# 使用pickle模块将数据对象保存到文件
+data1 = {'a': [1, 2.0, 3, 4+6j],
+         'b': ('string', u'Unicode string'),
+         'c': None}
+
+selfref_list = [1, 2, 3]
+selfref_list.append(selfref_list)
+
+output = open('data.pkl', 'wb')
+
+# Pickle dictionary using protocol 0.
+pickle.dump(data1, output)
+
+# Pickle the list using the highest protocol available.
+pickle.dump(selfref_list, output, -1)
+
+output.close()
 """##全文引号结束
